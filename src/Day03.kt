b387ed02f2dc659c482791e5596bfa8c8efe3f51
Paul <119668558+paulbonugli@users.noreply.github.com>
2024-12-03 21:02:42 +0000

    day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -4,12 +4,17 @@ fun main() {
     fun part1(input: List<String>) : Int {
         return Regex("mul\\((\\d+),(\\d+)\\)")
             .findAll(input.joinToString())
-            .map { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
-            .sum()
+            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
     }
 
     fun part2(input: List<String>) : Int {
-        return 0
+        return input.joinToString()
+            // split into fragments to be done
+            .split("do()")
+            // we can ignore everything in the fragment following a don't()
+            .map { fragment -> listOf(fragment.split("don't()")[0]) }
+            // what's left we can process and then sum
+            .sumOf(::part1)
     }
 
     val input = readInput("Day03")
